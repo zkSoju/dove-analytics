f2499e83906da81e03de8cb7987836c1334cffcc
soju <underrated@gmail.com>
2023-03-29 15:59:32 -0700

    style and hide some data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useAmmData from "@/lib/hooks/useAmmData";
+import { BiArrowToBottom } from "react-icons/bi";
 
 export default function Home() {
   const data = useAmmData();
@@ -14,6 +15,9 @@ export default function Home() {
             <thead>
               <tr className="text-left">
                 <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
+                  Type
+                </th>
+                <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
                   Balance 0
                 </th>
                 <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
@@ -26,47 +30,42 @@ export default function Home() {
                   Reserve 1
                 </th>
                 <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
-                  Reserve 0 Cumulative
-                </th>
-                <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
-                  Reserve 1 Cumulative
-                </th>
-                <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
                   Voucher 0
                 </th>
                 <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200">
                   Voucher 1
                 </th>
+                <th className="border-b border-slate-600 p-4 pt-0 pb-3 text-left font-medium text-slate-200"></th>
               </tr>
             </thead>
             <tbody className="bg-white/10">
               {data.map((item, id) => (
                 <tr key={id}>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeBalance0.toString()}→{item.balance0.toString()}
+                    <div className="flex w-12 items-center justify-center bg-yellow-400 py-1 text-xs uppercase tracking-wider text-black">
+                      Swap
+                    </div>
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeBalance1.toString()}→{item.balance1.toString()}
+                    {item.balance0.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeReserve0.toString()}→{item.reserve0.toString()}
+                    {item.balance1.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeReserve1.toString()}→{item.reserve1.toString()}
+                    {item.reserve0.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeReserve0Cumulative.toString()}→
-                    {item.reserve0Cumulative.toString()}
+                    {item.reserve1.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeReserve1Cumulative.toString()}→
-                    {item.reserve1Cumulative.toString()}
+                    {item.voucher0.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeVoucher0.toString()}→{item.voucher0.toString()}
+                    {item.voucher1.toString()}
                   </td>
                   <td className="border-b border-slate-800 p-4 text-slate-400">
-                    {item.beforeVoucher1.toString()}→{item.voucher1.toString()}
+                    <BiArrowToBottom className="text-white" />
                   </td>
                 </tr>
               ))}
