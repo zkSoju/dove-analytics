3ed7bcc5737bc0f1c670135e68e4c38031208768
soju <underrated@gmail.com>
2023-03-29 15:13:34 -0700

    style

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,41 @@ export default function Home() {
 
   return (
     <div>
-      <div className="relative z-10 flex h-screen w-full items-center justify-center">
-        <div className="flex w-full max-w-7xl flex-col items-start p-4">
-          <h1 className="text-2xl text-white">AMM Data</h1>
-          <table className="w-full table-auto text-white">
+      <div className="relative z-10 flex min-h-screen w-full items-center justify-center">
+        <div className="flex w-full max-w-6xl flex-col items-start pt-16">
+          <h1 className="mb-16 text-2xl text-white">AMM Data</h1>
+          <table className="w-full table-auto text-sm text-white">
             <thead>
               <tr className="text-left">
-                <th>Balance 0 Before</th>
-                <th>Balance 0 After</th>
-                <th>Balance 1 Before</th>
-                <th>Balance 1 After</th>
+                <th className="border-b p-4 pt-0 pb-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                  Balance 0
+                </th>
+                <th className="border-b p-4 pt-0 pb-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                  Balance 1
+                </th>
+                <th className="border-b p-4 pt-0 pb-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                  Reserve 0
+                </th>
+                <th className="border-b p-4 pt-0 pb-3 text-left font-medium text-slate-400 dark:border-slate-600 dark:text-slate-200">
+                  Reserve 1
+                </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="bg-white/10">
               {data.map((item, id) => (
                 <tr key={id}>
-                  <td>{item.beforeBalance0.toString()}</td>
-                  <td>{item.balance0.toString()}</td>
-                  <td>{item.beforeBalance1.toString()}</td>
-                  <td>{item.balance1.toString()}</td>
+                  <td className="border-b border-slate-800 p-4 text-slate-400">
+                    {item.beforeBalance0.toString()}→{item.balance0.toString()}
+                  </td>
+                  <td className="border-b border-slate-800 p-4 text-slate-400">
+                    {item.beforeBalance1.toString()}→{item.balance1.toString()}
+                  </td>
+                  <td className="border-b border-slate-800 p-4 text-slate-400">
+                    {item.beforeReserve0.toString()}→{item.reserve0.toString()}
+                  </td>
+                  <td className="border-b border-slate-800 p-4 text-slate-400">
+                    {item.beforeReserve1.toString()}→{item.reserve1.toString()}
+                  </td>
                 </tr>
               ))}
             </tbody>
